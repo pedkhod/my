9946f818eae6f4d6787f3968c8c85858a4997891
pedkhod <pkhoddami@gmail.com>
2020-04-16 01:39:36 +0430

    حذف داده های تکراری با تابع set

--- list_input.py
+++ list_input.py
@@ -4,11 +4,6 @@ for d in range(0,a) :
     s=input("enter your next item")
     my_list.append(s)
 print(my_list)
-for i in range(0,len(my_list)):
-    s=my_list[i]
-    j=i+1
-    if j> len(my_list):
-       for a in range(j,len(my_list)):
-           if my_list[a]==s:
-             my_list.pop(a)
+z=set(my_list)
+my_list=list(z)
 print(my_list)
