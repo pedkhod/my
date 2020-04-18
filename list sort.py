1b01f62498ddcb97a40006f43ce3b3d3ef8bc907
pedkhod <pkhoddami@gmail.com>
2020-04-18 13:48:08 +0430

    مرتبسازی با حلقه

--- list sort.py
+++ list sort.py
@@ -1,11 +1,14 @@
 a=int(input("enter a number"))
 my_list=[]
-while a!=[]:
+try:
+ while True:
    my_list.append(a)
    a=int(input("enter a number"))
-for i in range(0,len(my_list)):
+except:
+ for i in range(0,len(my_list)):
     for j in range(0,i):
         if my_list[i]>my_list[j]:
             a=my_list[j]
             my_list[j]=my_list[i]
             my_list[i]=a
+print(my_list)
