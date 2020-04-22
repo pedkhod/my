e40c6b89729d2cd67ed6714b2458444ac6a139d8
pedkhod <pkhoddami@gmail.com>
2020-04-23 03:26:46 +0430

    error

--- vnev/Scripts/files/redfiles.py
+++ vnev/Scripts/files/redfiles.py
@@ -1,7 +1,12 @@
 f= open('wde.txt' ,'r')
+s=open("email.txt","w+")
 for kh in f:
   kh=kh.rstrip()
   kh=kh.replace(" ",".")
   print(kh ,'888')
   kh=kh+"@ya.com"
   print(kh)
+  s.writelines(kh)
+f.close
+for s1 in s:
+    print(s1)
